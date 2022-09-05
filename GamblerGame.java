dd3ba8c07c3a54f96e0e23b5c41c1daa336984a9
ram-221 <rameshmalli857.com>
2022-09-05 19:43:26 +0530

    Every day Strat with stack of 00 & bet  Every Game

---
+++ GamblerGame.java
@@ -0,0 +1,16 @@
+package com.bridgelab;
+
+
+public class GamblerGame {
+
+	public static void main(String args[]) {
+		
+		double stack=100;
+		float bet = 1;
+
+		System.out.println("enter the every day stack: "+stack);
+	
+		System.out.println("day playing bet is: "+bet);
+		
+	}
+}
